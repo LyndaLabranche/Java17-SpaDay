15ac903c79790811aed095fd0fd031535365c49e
LyndaLabranche <lynda.labranche@gmail.com>
2023-10-04 23:36:46 -0700

    imported src files

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,16 @@
+package org.launchcode.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @PostMapping("/user/add")
+    public String displayAddUserForm(){
+        return "add.html";
+    }
+
+}
